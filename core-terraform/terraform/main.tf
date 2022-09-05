6bc4ace2c234cc84f1d749567c1d45e400b6db8d
Nikhil Tammana <ntammana@chewy.com>
2022-09-05 18:24:01 -0500

    feat: add lambda

--- core-terraform/terraform/main.tf
+++ core-terraform/terraform/main.tf
@@ -13,6 +13,41 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "${var.environment}-test-lambda"
+  role          = aws_iam_role.test_role.arn
+  handler       = "index.test"
+  runtime       = "nodejs14.x"
+
+  ephemeral_storage {
+    size = 10240 # Min 512 MB and the Max 10240 MB
+  }
+}
 
 resource "aws_dynamodb_table" "learning-db" {
   name           = "${var.environment}-learning-db"
